cdd3d13aecaf85db4a523a3b4a7d8b7ea8f34858
Thiago Cruz <thagocruz@gmail.com>
2018-02-07 02:17:26 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 angular.module("listaTelefonica", []);
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope) {
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, uppercaseFilter) {
 	$scope.app = "Lista Telefonica";
 	$scope.contatos = [
-		{nome: "Thiago", telefone: 99999999, cor: "blue"},
-		{nome: "Maria", telefone: 99999998, cor: "red"},
-		{nome: "João", telefone: 99999997, cor: "green"},
-		{nome: "José", telefone: 99999996, cor: "black"}
+		{nome: uppercaseFilter("Thiago"), telefone: 99999999, data: new Date()},
+		{nome: "Maria", telefone: 99999998, data: new Date()},
+		{nome: "João", telefone: 99999997, data: new Date()},
+		{nome: "José", telefone: 99999996, data: new Date()}
 	];
 	$scope.operadoras = [
-		{nome: "Oi", codigo: 14, categoria:"Celular"},
-		{nome: "Tim", codigo: 15, categoria:"Celular"},
-		{nome: "Vivo", codigo: 16, categoria:"Celular"},
-		{nome: "Claro", codigo: 17, categoria:"Celular"},
-		{nome: "Embratel", codigo: 18, categoria:"Fixo"},
-		{nome: "Telemar", codigo: 19, categoria:"Fixo"}
+		{nome: "Oi", codigo: 14, categoria:"Celular", preco: 1},
+		{nome: "Tim", codigo: 15, categoria:"Celular", preco: 1},
+		{nome: "Vivo", codigo: 16, categoria:"Celular", preco: 2},
+		{nome: "Claro", codigo: 17, categoria:"Celular", preco: 2},
+		{nome: "Embratel", codigo: 18, categoria:"Fixo", preco: 3},
+		{nome: "Telemar", codigo: 19, categoria:"Fixo", preco: 3}
 	];
 	$scope.adicionarContato = function (contato){
 		$scope.contatos.push(angular.copy(contato));
 		delete $scope.contato;
+		$scope.contatoForm.$setPristine();
 	};
 	$scope.apagarContatos = function (contatos) {
 		$scope.contatos = contatos.filter(function (contato){
@@ -29,4 +30,8 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
 			return contato.selecionado;
 		});
 	};
+	$scope.ordenarPor = function(campo){
+		$scope.criterioDeOrnacao = campo;
+		$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+	}
 }); 
