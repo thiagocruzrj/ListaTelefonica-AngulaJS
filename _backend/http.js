dab198324381decf39039b87aa9dba42ea488e13
Thiago Cruz <thagocruz@gmail.com>
2018-03-09 03:07:05 -0300

    Update http.js

--- _backend/http.js
+++ _backend/http.js
@@ -11,11 +11,11 @@ var operadoras = [
 ];
 
 var contatos = [
-    {nome: "Thiago", telefone: "9999-8888", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
-    {nome: "Ana", telefone: "9999-8877", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
-    {nome: "Maria", telefone: "9999-8866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}},
-    {nome: "Isabella", telefone: "9999-8866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}},
-    {nome: "João", telefone: "9999-8866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
+    {nome: "thiago da cruz", telefone: "9999-8888", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
+    {nome: "ana da silva", telefone: "9999-8877", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
+    {nome: "maria de paula", telefone: "9999-8866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}},
+    {nome: "isabella nunes", telefone: "9999-8866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}},
+    {nome: "joão fernando", telefone: "9999-8866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
 ];
 
 app.interceptor(function (req, res, next) {
